d90f3318c245356986d219b1756bc58a8a305c48
Eum Young Hyun <opwer032@naver.com>
2021-10-04 21:02:05 +0900

    feat: Init wav component

---
+++ react_test/src/wav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Wav() {
+  return <Container> </Container>;
+}
+
+export default Wav;
+
+const Container = styled.div`
+  border: 3px solid red;
+`;
