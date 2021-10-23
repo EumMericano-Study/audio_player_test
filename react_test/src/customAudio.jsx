79c4277f2e83b1dee59784ca5fe1c5f8e6e622e4
Eum Young Hyun <opwer032@naver.com>
2021-10-23 20:45:47 +0900

    feat: Add button on audio

--- react_test/src/customAudio.jsx
+++ react_test/src/customAudio.jsx
@@ -7,13 +7,35 @@ function CustomAudio() {
   const [audioSrc] = useState(new Audio(audio1));
   const [isLoading, setLoading] = useState(true);
 
+  const [isPlay, setIsPlay] = useState(
+    audioSrc.paused && audioSrc.currentTime > 0 && !audioSrc.ended
+  );
   useEffect(() => {
     audioSrc.addEventListener("loadeddata", () => {
       setLoading(false);
     });
   });
 
-  return <Container>{!isLoading && <audio src={audio1} controls />}</Container>;
+  function play() {
+    if (!isPlay) {
+      audioSrc.play();
+      setIsPlay(true);
+      return;
+    }
+    audioSrc.pause();
+    setIsPlay(false);
+  }
+
+  return (
+    <Container>
+      {!isLoading && (
+        <>
+          <audio src={audio1} />
+          <button onClick={play}>재생</button>
+        </>
+      )}
+    </Container>
+  );
 }
 
 export default CustomAudio;
